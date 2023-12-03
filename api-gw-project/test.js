98004126cb57f897074070e3af6635aaf1c47056
dongha kim <kimdongha15@naver.com>
2023-12-03 22:58:32 +0900

    add api gateway config

--- api-gw-project/test.js
+++ api-gw-project/test.js
@@ -1,4 +1,11 @@
-module.exports.handler = async (event) => {
+'use strict';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { PutCommand, ScanCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+
+export const main = async (event) => {
+  console.log(event);Z
+//  putItem(event);
+  
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -10,4 +17,19 @@ module.exports.handler = async (event) => {
       2
     ),
   };
+
+  // const putItem = async (params) => {
+  //   const client = new DynamoDBClient({});
+  //   const translateConfig = { marshallOptions: { convertClassInstanceToMap: true } };
+  //   const ddbDocClient = DynamoDBDocumentClient.from(client, translateConfig);
+
+  //   const command = new PutCommand({
+  //     TableName: DDB_TABLE,
+  //     Item: { id: params },
+  //   });
+  
+  //   const response = await ddbDocClient.send(command);
+  //   console.log('DDB Put Item:', JSON.stringify({ input: { id: params }, result: response }));
+  //   return response;
+  // };
 };
