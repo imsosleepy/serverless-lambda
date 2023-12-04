8ed799f931901482599c26be72cd357c189ca4df
dongha kim <kimdongha15@naver.com>
2023-12-04 17:43:43 +0900

    fix : es5 코드로 수정

--- api-gw-project/index.js
+++ api-gw-project/index.js
@@ -1,16 +1,34 @@
-'use strict';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { PutCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { PutCommand, DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
 
-export const main = async (event) => {
-  const { param } = event.pathParameters;
-  await putItem(param, event);
+const putItem = async (param, event) => {
+    const client = new DynamoDBClient({});
+    const translateConfig = { marshallOptions: { convertClassInstanceToMap: true } };
+    const ddbDocClient = DynamoDBDocumentClient.from(client, translateConfig);
+
+    const command = new PutCommand({
+      TableName: 'serverless-ddb-dev',
+      Item: { id: param, event: event },
+    });
   
+    const response = await ddbDocClient.send(command);
+    console.log('DDB Put Item:', JSON.stringify({ input: { id: param, event: event }, result: response }));
+    return response;
+  };
+
+module.exports.handler = async (event) => {
+    try {
+      const { param } = event.pathParameters;
+      const response = await putItem(param, event);
+      console.log("DynamoDB response:", response);
+  } catch (error) {
+    console.error("Error saving to DynamoDB:", error);
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
+        message: 'Go Serverless v3.0! Your function executed successfully!!!!!!',
         input: event,
       },
       null,
@@ -19,17 +37,4 @@ export const main = async (event) => {
   };
 };
 
-const putItem = async (param, event) => {
-  const client = new DynamoDBClient({});
-  const translateConfig = { marshallOptions: { convertClassInstanceToMap: true } };
-  const ddbDocClient = DynamoDBDocumentClient.from(client, translateConfig);
-
-  const command = new PutCommand({
-    TableName: 'serverless-ddb-dev',
-    Item: { id: param, event: event },
-  });
-
-  const response = await ddbDocClient.send(command);
-  console.log('DDB Put Item:', JSON.stringify({ input: { id: param, event: event }, result: response }));
-  return response;
-};
+module.exports.putItem = putItem;
