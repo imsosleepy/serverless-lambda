85486c7782a02b667aaab16802d30c08de4aabcd
dongha kim <kimdongha15@naver.com>
2023-12-04 17:39:17 +0900

    feat : 다이나모DB 연동

--- api-gw-project/index.js
+++ api-gw-project/index.js
@@ -1,10 +1,10 @@
 'use strict';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { PutCommand, ScanCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { PutCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 export const main = async (event) => {
-  console.log(event);
-//  putItem(event);
+  const { param } = event.pathParameters;
+  await putItem(param, event);
   
   return {
     statusCode: 200,
@@ -17,19 +17,19 @@ export const main = async (event) => {
       2
     ),
   };
+};
 
-  // const putItem = async (params) => {
-  //   const client = new DynamoDBClient({});
-  //   const translateConfig = { marshallOptions: { convertClassInstanceToMap: true } };
-  //   const ddbDocClient = DynamoDBDocumentClient.from(client, translateConfig);
+const putItem = async (param, event) => {
+  const client = new DynamoDBClient({});
+  const translateConfig = { marshallOptions: { convertClassInstanceToMap: true } };
+  const ddbDocClient = DynamoDBDocumentClient.from(client, translateConfig);
 
-  //   const command = new PutCommand({
-  //     TableName: DDB_TABLE,
-  //     Item: { id: params },
-  //   });
-  
-  //   const response = await ddbDocClient.send(command);
-  //   console.log('DDB Put Item:', JSON.stringify({ input: { id: params }, result: response }));
-  //   return response;
-  // };
+  const command = new PutCommand({
+    TableName: 'serverless-ddb-dev',
+    Item: { id: param, event: event },
+  });
+
+  const response = await ddbDocClient.send(command);
+  console.log('DDB Put Item:', JSON.stringify({ input: { id: param, event: event }, result: response }));
+  return response;
 };
